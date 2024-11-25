84e4b074b3d42b1b08bc3731a984ba456148a807
Ishita Dey <144545432+Is-hitaDey@users.noreply.github.com>
2024-11-25 14:14:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let val = Math.floor(Math.random()*100)+1;
+alert("WELCOME to Guess the Number game");
+alert("The Random number is chosen now its your turn...")
+let guess=-1;
+let count=0;
+while (guess!=0){
+    let guess= prompt("Enter the number:");
+    if (guess>val){
+        alert("Enter a lower number:");
+        count+=1;
+    }else if (guess<val){
+        alert("Enter a higher number:");
+        count+=1;
+    }else{
+        alert("You got it correct!!")
+        count+=1;
+        break;
+    }
+}
+alert("The number was "+ val);
+alert("You have taken "+count+" guesses");
